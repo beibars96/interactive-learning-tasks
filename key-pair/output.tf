4430d82d395a25b22704df8fcaf33b09b6d35ce4
tangerkulov <b.tangerkulov@gmail.com>
2021-11-17 05:56:06 +0000

    we should finish soon!

---
+++ key-pair/output.tf
@@ -0,0 +1,12 @@
+// output "key_name" {
+//   value = aws_key_pair.deployer.key_name
+// }
+
+output {
+  value = aws_key_pair.deployer.key_name
+}
+
+
+// output "region" {
+//   value = aws_key_pair.region
+// }
