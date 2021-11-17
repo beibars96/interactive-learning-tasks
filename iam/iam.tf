3c9eb5461535f810b16432138f92287e452977e5
tangerkulov <b.tangerkulov@gmail.com>
2021-11-17 04:47:28 +0000

    added some codes

---
+++ iam/iam.tf
@@ -0,0 +1,22 @@
+resource "aws_iam_user" "bob" {
+  name = "bob"
+  tags = {
+    Team = "DevOps"
+  }
+}
+
+
+resource "aws_iam_group" "sysusers" {
+  name = "sysusers"
+}
+
+
+
+
+resource "aws_iam_group_membership" "team" {
+  name = "sysusers"
+  users = [
+    aws_iam_user.bob.name,
+  ]
+  group = aws_iam_group.sysusers.name
+}
